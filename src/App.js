29a6450e1ed761a27933aa7c29b8de9d13def7b5
VishvaViz <vishvabulls@gmail.com>
2023-06-17 05:52:52 -0400

    finalcommit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import ChatBot from './Comp/ChatBot';
 
 function App() {
-    return (<React.Fragment>
+    return <React.Fragment>
         <ChatBot/>   
-    </React.Fragment>);
-}
-
+    </React.Fragment>
+};
 export default App;
